0205d7161c31875e37fac73c35f6cc3c8063db70
joshshow <jcshowalter@liberty.edu>
2015-03-03 13:43:22 -0500

    Added additional commands to ftp.py
    Added at least 5 more commands to ftp.py that are working properly

--- ftp.py
+++ ftp.py
@@ -1,9 +1,18 @@
 #!/bin/python
 from ftplib import FTP
+
+def callback(data):
+	print data
+	print
+
 ftp = FTP('ftp.drwestfall.net')
-ftp.login(user='ftp03', passwd='student')
-ftp.retrlines('LIST')
-ftp.mkd('newdir')
+ftp.login(user='ftp03', passwd='student')    # login into the ftp site with the correct credentials
+ftp.getwelcome() 	# does not do anything at the moment
+ftp.retrlines('LIST') 	# Lists the contents of the directory
+ftp.retrlines('RETR test.txt', callback) 	#returns the contents of the file. 
+ftp.storlines('STOR newfile.txt', open('newfile.txt')) 	 # uploads the file to the server
+ftp.retrlines('RETR newfile.txt', callback)
+
 ftp.quit()
 
 
